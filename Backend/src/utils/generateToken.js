78f947c9f6b9eef62caaa68469be5e121a8e104f
= <=>
2026-08-14 16:37:19 +0500

    change cookie expiry

--- Backend/src/utils/generateToken.js
+++ Backend/src/utils/generateToken.js
@@ -9,7 +9,7 @@ const generateToken = (res, userId) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
     sameSite: 'lax',
-    maxAge: 7 * 24 * 60 * 60 * 1000,
+    maxAge: 1 * 24 * 60 * 60 * 1000,
   });
 
   return token;
